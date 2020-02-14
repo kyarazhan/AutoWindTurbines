317f55c455a3818dab6dc3122ab204f2d658f30c
kyarazhan <shangrilas@live.com>
2020-02-14 23:54:53 +0800

    小改动

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#from Coords import*
+from Coords import*
 #from DistanceSet import*
 #from RowColumn import*
 #from WindTurbine import*
-from AutoWindTurbines.Coords import XYZ
+#from Coords import XYZ
 
 XYZ()
 #zz = x0+y0
 #print(zz)
-print("二次打印 X%d"%x0 +"  " + "Y%d"%y0)
+#print("二次打印 X%d"%x0 +"  " + "Y%d"%y0)
