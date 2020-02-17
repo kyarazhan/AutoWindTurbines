b7d4a12f20f2f558133b5986891815894bf4ecbf
kyarazhan <shangrilas@live.com>
2020-02-17 10:14:00 +0800

    bug修复

--- WindTurbine.py
+++ WindTurbine.py
@@ -1,19 +1,21 @@
 
 class windturbine():
-    print('请输入风机参数:')
+    print('请输入风机参数')
     global hub
     global dia
-    print('\n请分别输入风机轮毂直径及高度，整数即可，以空格键隔开')
+    print('\n请分别输入风机桨叶直径及轮毂高度，整数即可，以空格键隔开')
     while True:
         try:
-            hub,dia = map(int,input().split())
+            dia,hub = map(int,input().split())
             if hub > 200 or hub < 0:
                 print('输入有误！请重新输入！')
                 continue
             elif dia > 200 or dia < 100:
                 print('输入有误！请重新输入！')
                 continue
-            #return hub, dia
+            elif dia > 2*hub:
+                print('桨叶触地！')
+                continue
             break
         except ValueError:
             print('输入有误！请重新输入')
