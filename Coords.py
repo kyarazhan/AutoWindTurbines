4e879b0bf3fc4191b83775abbf4fac89d4c43370
kyarazhan <shangrilas@live.com>
2020-02-18 21:53:08 +0800

    bug修复

--- Coords.py
+++ Coords.py
@@ -19,38 +19,5 @@ class XYZ():
             print('输入有误！请重新输入')
 
 
-
-"""        
-        x0 = input("请输入X坐标：\n")
-        try:
-            if x0 == 0 :
-                print("坐标不能全为 〇 ，请重新输入！")
-                continue
-            elif 8 != len(str(x0)):
-                print("X坐标为8位数，请重新输入！")
-                continue
-            elif False == x0.isdigit() :
-                print("输入有误！")
-                continue
-        except ValueError:
-            print("输入有误！")
-            continue
-        y0 = input("请输入Y坐标：\n")
-        try:
-            if y0 == 0:
-                print("坐标不能全为 〇 ，请重新输入！")
-                continue
-            elif 7 != len(str(y0)):
-                print("Y坐标为7位数，请重新输入！")
-                continue
-            elif False == y0.isdigit():
-                print("输入有误！")
-                continue
-        except ValueError:
-            print("输入有误！")
-            continue
-        break
-"""
-
 if __name__ == '__main__':
     XYZ()
--- main.py
+++ main.py
@@ -11,21 +11,23 @@ gg = DistanceSet.left_right
 print(str(zz))
 print("mian打印X%s"%hh + "  " + "main打印Y%s"%gg)
 
-"""
 def calculate2():
     zz = DistanceSet.front_back + DistanceSet.left_right
     hh = DistanceSet.front_back
     gg = DistanceSet.left_right
     print(str(zz))
     print("mian打印X%s" % hh + "  " + "main打印Y%s" % gg)
+
+"""
 def calculate():
     nu = 0
 
     for i in range(RowColumn.row):
-        for j in  range(RowColumn.column):
+        for j in range(RowColumn.column):
             t = nu+1
             nu += 1
-            result = 'T{} {} {}\t'.format(str(t),str(Coords.x0+i*WindTurbine.hub*DistanceSet.front_back),str(Coords.y0+j*WindTurbine.hub*DistanceSet.left_right))
+            result = 'T{} {} {}\t'.format(str(t),str(Coords.x0+i*WindTurbine.hub*DistanceSet.front_back),\
+                                          str(Coords.y0+j*WindTurbine.hub*DistanceSet.left_right))
 
             print("外部打印%s"%result)
 
@@ -33,12 +35,5 @@ def calculate():
                 file_handle.write(result)
                 file_handle.write('\n')
 
-
 if __name__=='__main__':
     calculate()
-
-
-
-
-
-
